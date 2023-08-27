1e02b936341aa719a8b2cd9725c9eb2ed978591a
Kyle Zhike Chen <zk.chan007@gmail.com>
2023-08-27 15:13:00 +0000

    add source

--- models/fct_customer_orders.sql
+++ models/fct_customer_orders.sql
@@ -6,20 +6,20 @@ WITH paid_orders as (select Orders.ID as order_id,
     p.payment_finalized_date,
     C.FIRST_NAME    as customer_first_name,
         C.LAST_NAME as customer_last_name
-FROM kyle-test-384716.dbt_kzhikechen.jaffle_shop_orders as Orders
+FROM {{ source('jaffle_shop', 'jaffle_shop_orders') }} as Orders
 left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-        from kyle-test-384716.dbt_kzhikechen.stripe_payments
+        from {{ source('stripe', 'stripe_payments') }}
         where STATUS <> 'fail'
         group by 1) p ON orders.ID = p.order_id
-left join kyle-test-384716.dbt_kzhikechen.jaffle_shop_customers C on orders.USER_ID = C.ID ),
+left join {{ source('jaffle_shop', 'jaffle_shop_customers') }} C on orders.USER_ID = C.ID ),
 
 customer_orders 
 as (select C.ID as customer_id
     , min(ORDER_DATE) as first_order_date
     , max(ORDER_DATE) as most_recent_order_date
     , count(ORDERS.ID) AS number_of_orders
-from kyle-test-384716.dbt_kzhikechen.jaffle_shop_customers C 
-left join kyle-test-384716.dbt_kzhikechen.jaffle_shop_orders as Orders
+from {{ source('jaffle_shop', 'jaffle_shop_customers') }} C 
+left join {{ source('jaffle_shop', 'jaffle_shop_orders') }} as Orders
 on orders.USER_ID = C.ID 
 group by 1)
 
--- models/staging/stripe/stg_payments.sql
+++
@@ -1,9 +0,0 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    amount,
-    created
-
-from {{ source ('stripe', 'payment') }}
