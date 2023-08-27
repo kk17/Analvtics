143bb2f08d0b99ffca2947299bb75fe97249f8d9
Kyle Zhike Chen <zk.chan007@gmail.com>
2023-08-27 15:55:14 +0000

    a

--- models/fct_customer_orders.sql
+++ models/fct_customer_orders.sql
@@ -30,9 +30,6 @@ completed_payments as (
     group by 1
 ),
 
--- Final CTE
--- Simple Select Statment
-
 paid_orders as (
     select
         orders.id as order_id,
@@ -44,11 +41,8 @@ paid_orders as (
         c.first_name as customer_first_name,
         c.last_name as customer_last_name
     from orders
-    left join
-        completed_payments as p on orders.id = p.order_id
-    left join
-        customers as  c
-        on orders.user_id = c.id
+    left join completed_payments as p on orders.id = p.order_id
+    left join customers as c on orders.user_id = c.id
 ),
 
 customer_orders as (
@@ -67,29 +61,21 @@ customer_orders as (
 -- final CTE
 final as (select
     p.*,
+    -- sales transaction sequence
     row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (
-        partition by customer_id order by p.order_id
-    ) as customer_sales_seq,
+    -- customer sales sequence
+    row_number() over ( partition by customer_id order by p.order_id) as customer_sales_seq,
     case
         when c.first_order_date = p.order_placed_at then 'new' else 'return'
     end as nvsr,
-    x.clv_bad as customer_lifetime_value,
+	-- customer lifetime value
+    sum(total_amount_paid) over (
+        partition by paid_orders.customer_id
+        ) as customer_lifetime_value,
+    -- first day of sale
     c.first_order_date as fdos
-from paid_orders p
+from paid_orders as p
 left join customer_orders as c using (customer_id)
-left outer join
-    (
-        select p.order_id, sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join
-            paid_orders t2
-            on p.customer_id = t2.customer_id
-            and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ) x
-    on x.order_id = p.order_id
 order by order_id
 )
 
